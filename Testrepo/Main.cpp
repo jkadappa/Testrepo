c6b28b3c5cc543744afa28ab230cfa530d3253a5
kadappan jothi <jkadappa@syr.edu>
2013-05-15 14:46:54 -0400

    Intial Draft
    I have created the draft code to perform the calculation operation

---
+++ Testrepo/Main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+//#include "useradd1.h"
+
+void add(int a,int b)
+{
+	std::cout<<"the sum of two numbers"<<a+b<<std::endl; 
+}
+void sub(int a,int b)
+{
+	std::cout<<"the sum of two numbers"<<a-b<<std::endl; 
+}
+void mul(int a,int b)
+{
+	std::cout<<"the sum of two numbers"<<a*b<<std::endl; 
+}
+
+
+
+int main(int argc, char * argv[])
+{
+	add(2,2);
+	
+
+}
