d259a240d1be084a16e5d37a48cdb5880fdde78c
mohitb117 <mbhalla@syr.edu>
2013-05-15 14:51:24 -0400

    MOhit's Commit

--- Testrepo/Main.cpp
+++ Testrepo/Main.cpp
@@ -3,7 +3,7 @@
 
 void add(int a,int b)
 {
-	std::cout<<"the sum of two numbers"<<a+b<<std::endl; 
+	std::cout<<"the sum of two numbers"<<a+b<<std::endl; 	
 }
 void sub(int a,int b)
 {
@@ -19,6 +19,6 @@ void mul(int a,int b)
 int main(int argc, char * argv[])
 {
 	add(2,2);
-	
+	sub(2,2);	
 
 }
