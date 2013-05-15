ce8e77e9b720fe20617f2dbee18b9104708234f3
Arun <arnaraya@syr.edu>
2013-05-15 15:30:04 -0400

    modifyArun

--- Testrepo/Main.cpp
+++ Testrepo/Main.cpp
@@ -18,15 +18,16 @@ void mul(int a,int b)
 
 int main(int argc, char * argv[])
 {
-	add(2,2);
-	sub(2,2);
-    mul(2,2);
+add(2,2);
+sub(2,2);
+mul(2,2);
 add(5,6);
 sub(9,7);
 mul(9,9);
     
-    add(12,13);
-	sub(10,10);
+add(12,13);
+sub(10,10);
 
 
+xyc(10,43);
 }
