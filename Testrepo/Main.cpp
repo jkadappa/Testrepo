f7071d8b9ac339c1d9122534d31136c493a0da37
mohitb117 <mbhalla@syr.edu>
2013-05-16 01:40:01 -0400

    ConflictResolve

--- Testrepo/Main.cpp
+++ Testrepo/Main.cpp
@@ -18,39 +18,24 @@ void mul(int a,int b)
 
 int main(int argc, char * argv[])
 {
-<<<<<<< HEAD
 	add(2,2);
 	sub(2,2);
     mul(2,2);
 
-=======
-add(2,2);
-sub(2,2);
-mul(2,2);
->>>>>>> master
 add(5,6);
 sub(9,7);
 mul(9,9);
     
-<<<<<<< HEAD
     add(12,13);
 	sub(10,10);
     mul(10,11);
-=======
-add(12,13);
-sub(10,10);
->>>>>>> master
 
 add(6,5);
 sub(7,9);
 mul(1,81);
 
-<<<<<<< HEAD
-    
-=======
 xyc(10,43);
 sub(10,10);
 sub(101,110);
 
->>>>>>> master
 }
